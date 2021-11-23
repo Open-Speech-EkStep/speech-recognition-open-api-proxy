f8c870e90ec7fdbd47f6af4ca653ca965c1fd6a5
Niresh Kumar R <nireshkumarr@Nireshs-MacBook-Pro.local>
2021-11-24 01:15:21 +0530

    fix: metadata support fixed

--- src/proxy/grpc_client.js
+++ src/proxy/grpc_client.js
@@ -1,4 +1,4 @@
-const {getLanguageIpMap} = require('./language_config');
+const { getLanguageIpMap } = require('./language_config');
 let languageIpMap = getLanguageIpMap();
 const grpc = require("grpc");
 grpc.max_send_message_length = 50 * 1024 * 1024;
@@ -19,7 +19,7 @@ let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 let proto = grpc.loadPackageDefinition(packageDefinition).ekstep.speech_recognition;
 
 class GrpcClient {
-    constructor(language){
+    constructor(language) {
         this.language = language;
         this.client = null;
         this.stream = null;
@@ -29,7 +29,7 @@ class GrpcClient {
         let language = this.language;
         return languageIpMap[language];
     }
-    
+
     #getGrpcClient() {
         let grpc_ip = this.#getGrpcIp();
         let grpc_client = new proto.SpeechRecognizer(
@@ -39,7 +39,7 @@ class GrpcClient {
         return grpc_client;
     }
 
-    connect(){
+    connect() {
         this.client = this.#getGrpcClient();
     }
 
@@ -47,18 +47,20 @@ class GrpcClient {
         let meta = new grpc.Metadata();
         meta.add('language', this.language);
         return new Promise((resolve, reject) => {
-            this.client.punctuate(msg,meta, (error, response) => {
+            this.client.punctuate(msg, meta, (error, response) => {
                 if (error) { reject(error); }
                 resolve(response);
             });
         });
     }
 
-    startStream(responseListener = () => {}, errorListener = () => {}){
-        if(this.client !== null && this.client !== undefined){
-            let meta = new grpc.Metadata();
-            meta.add('language', this.language);
-            this.stream = this.client.recognize_audio(meta)
+    startStream(responseListener = () => { }, errorListener = () => { }) {
+        if (this.client !== null && this.client !== undefined) {
+            let metadata = new grpc.Metadata();
+            metadata.add('language', this.language);
+            this.stream = this.client.recognize_audio(metadata, function (err, result) {
+                console.log("Stream connection result => ", result);
+            })
             this.stream.on("data", responseListener);
             this.stream.on("error", errorListener);
         } else {
@@ -66,27 +68,27 @@ class GrpcClient {
         }
     }
 
-    writeToStream(message){
-        if(this.stream !== null && this.stream !== undefined)
+    writeToStream(message) {
+        if (this.stream !== null && this.stream !== undefined)
             this.stream.write(message)
         else
             console.error("Stream not started/available")
     }
 
-    stopStream(){
-        if(this.stream !== null && this.stream !== undefined)
+    stopStream() {
+        if (this.stream !== null && this.stream !== undefined)
             this.stream.end()
         else
             console.error("Stream not started/available")
     }
 
     disconnect() {
-        if(this.client !== null && this.client !== undefined)
+        if (this.client !== null && this.client !== undefined)
             grpc.closeClient(this.client);
         else
             console.error("grpc client not available")
     }
-    
+
 }
 
 module.exports = GrpcClient;
